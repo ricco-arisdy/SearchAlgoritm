08aef8aa5c35bc1fbb19359b5c23cd9a2ceae330
riccoarisdy <riccoarisdy@gmail.com>
2022-10-24 17:05:17 +0700

    membuat binaryserach, lowerbound dan upperbpund

--- SearchAlgoritm/Program.cs
+++ SearchAlgoritm/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -34,8 +35,51 @@ namespace SearchAlgorithm
             Console.WriteLine("-----------------------------");
             for (i = 0; i < n; i++)
             {
+                Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                arr[i] = Int32.Parse(s1);
+            }
+        }
+        public void BinarySearch()
+        {
+            char ch;
+            do
+            {
+                // accept the number to be searched
+                Console.Write("\nEnter element want you to serach :");
+                int item = Convert.ToInt32(Console.ReadLine());
+
+                // applay binary search
+                int lowerbound = 0;
+                int upperbound = n - 1;
+
+                // obtain the index of the middle elements
+                int mid = (lowerbound + upperbound) / 2;
+                int ctr = 1;
+
+                //loop to search for the elements in the array
+                while ((item != arr[mid]) && (lowerbound <= upperbound))
+                {
+                    if (item > arr[mid])
+                        lowerbound = mid + 1;
+                    else
+                        upperbound = mid - 1;
+                    mid = (lowerbound + upperbound) / 2;
+                    ctr++;
+                }
+                if (item == arr[mid])
+                    Console.WriteLine("\n" + item.ToString() + "found at position" + (mid + 1).ToString());
+                else
+                    Console.WriteLine("\n" + item.ToString() + " not found in the array\n");
+                Console.WriteLine("\nNumber of compersion : " + ctr);
+
+                Console.WriteLine("\nCountinue serch (y/n):");
+                ch = char.Parse(Console.ReadLine());
+            } while ((ch == 'y') || (ch == 'Y'));
 
             }
+            
+            
         }
     }
 }

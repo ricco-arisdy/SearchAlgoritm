1c2702472d8d4a946ebe8a59ef99e919e476152a
riccoarisdy <riccoarisdy@gmail.com>
2022-10-24 16:26:55 +0700

    membuat public void dan membuat element array

--- SearchAlgoritm/Program.cs
+++ SearchAlgoritm/Program.cs
@@ -15,6 +15,19 @@ namespace SearchAlgorithm
         //Get the number of element to store in the array
         int i;
 
-
+        public void input()
+        {
+            while (true)
+            {
+                Console.Write("Enter the number of element in the array");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if ((n > 0) && (n <= 20))
+                break;
+                else
+                    Console.WriteLine("\nArray should have minimum 1 and maximum 20 element.\n");
+                
+            }
+        }
     }
 }

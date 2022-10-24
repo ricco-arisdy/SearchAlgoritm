e4b67b67ce30494015cf75f16d21ac75452d188a
riccoarisdy <riccoarisdy@gmail.com>
2022-10-24 16:13:28 +0700

    Add project files.

---
+++ SearchAlgoritm/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchAlgorithm
+{
+    class Program
+    {
+        //Array yang akan dicari
+        int[] aar = new int[20];
+        //Number of element in the array
+        int n;
+        //Get the number of element to store in the array
+        int i;
+    }
+}
